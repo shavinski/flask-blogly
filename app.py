662cb10780762e7f6bea11548f51571b9dbff1db
MKim <mrn.kim07@gmail.com>
2023-04-26 16:16:02 -0700

    add /users/new roues in app.py

--- app.py
+++ app.py
@@ -23,6 +23,23 @@ def show_home():
 @app.get('/users')
 def show_allUsers():
 
-    return render_template("all-users.html")
+    return render_template("all_users.html")
 
+@app.get('/users/new')
+def show_form():
 
+    return render_template("new_user_form.html")
+
+@app.post('/users/new')
+def add_user():
+    #grab data from user input
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    image_url = request.form['image_url']
+
+    #add a new user
+    user = User (first_name=first_name, last_name=last_name, image_url=image_url)
+    db.session.add(user)
+    db.session.commit()
+
+    return redirect('/users')
