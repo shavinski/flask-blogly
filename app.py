4020849fecbb0a99065a7394165407ad1f28638c
Jakob <shavinski.jakob@gmail.com>
2023-04-26 12:33:43 -0700

    add root route and templates

--- app.py
+++ app.py
@@ -2,8 +2,9 @@
 
 import os
 
-from flask import Flask
-from models import connect_db
+from flask import Flask, request, redirect, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
@@ -12,3 +13,16 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
+
+@app.get("/")
+def show_home():
+    """ Display home page """
+
+    return redirect('/users')
+
+@app.get('/users')
+def show_allUsers():
+
+    return render_template("all-users.html")
+
+
