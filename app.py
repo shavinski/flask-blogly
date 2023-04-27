4878a0b3c586aad276ef0bc35c38ebeefa912951
Jakob <shavinski.jakob@gmail.com>
2023-04-26 17:01:52 -0700

    add edit feature, work on delete feature

--- app.py
+++ app.py
@@ -23,7 +23,8 @@ def show_home():
 @app.get('/users')
 def show_allUsers():
 
-    return render_template("all_users.html")
+    users = User.query.all()
+    return render_template("all_users.html", users = users)
 
 @app.get('/users/new')
 def show_form():
@@ -43,3 +44,54 @@ def add_user():
     db.session.commit()
 
     return redirect('/users')
+
+@app.get('/users/<int:user_id>')
+def load_userid_page(user_id):
+
+    user = User.query.get(user_id)
+
+
+    #TODO: Should we have only have the instance here, or should we have first_name = user.first_name, last_name = user.last_name etc
+    return render_template('user_detail.html', user = user)
+
+@app.get('/users/<int:user_id>/edit')
+def load_user_edit(user_id):
+
+    user = User.query.get(user_id)
+
+    return render_template('edit_user.html', user = user)
+
+@app.post('/users/<int:user_id>/edit')
+def submit_updated_info(user_id):
+
+    user = User.query.get(user_id)
+
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    image_url = request.form['image_url']
+
+
+    user.first_name = first_name
+    user.last_name = last_name
+    user.image_url = image_url
+
+    db.session.add(user)
+    db.session.commit()
+
+    return redirect('/users')
+
+@app.post('/users/<int:user_id>/delete')
+def delete_user(user_id):
+
+    user = User.query.get(user_id)
+
+    db.session.delete(user)
+    db.session.commit()
+
+    return redirect('/users')
+
+# @app.get('/users/<int:user_id>/delete')
+# def reload_users_page(user_id):
+    
+
+
